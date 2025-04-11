3b7ab284b33ff2bde80c649ed671b0c9fc70d696
fledxy <trinhthienbao2003@gmail.com>
2025-04-11 19:43:08 +0700

    commit CI/CD

--- backend.tf
+++ backend.tf
@@ -1,18 +1,18 @@
-# # terraform { 
-# #   cloud { 
+# terraform { 
+#   cloud { 
     
-# #     organization = "fledxy" 
+#     organization = "fledxy" 
 
-# #     workspaces { 
-# #       name = "dev" 
-# #     } 
-# #   } 
-# # }
-
-# terraform {
-#   backend "s3" {
-#     bucket = "fledxytfbucket"
-#     key    = "dev-tf.tfstate"
-#     region = "ap-southeast-1"
-#   }
+#     workspaces { 
+#       name = "dev" 
+#     } 
+#   } 
 # }
+
+terraform {
+  backend "s3" {
+    bucket = "fledxytfbucket"
+    key    = "dev-tf.tfstate"
+    region = "ap-southeast-1"
+  }
+}
